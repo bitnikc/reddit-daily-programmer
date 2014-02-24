6cd7275419864516a6568b54a12c537577995c14
Nicolas KC <nicolas@kolnescassis.com>
2014-02-24 23:37:56 +0100

    Added Fredriks Oppgave (count letters in sentence), finished

---
+++ fredrikOppgave/FredriksOppgave.java
@@ -0,0 +1,29 @@
+package fredrikOppgave;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class FredriksOppgave {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String text;
+		List<LetterOccurance> letters = new ArrayList<LetterOccurance>();
+		
+		System.out.println("Write a text to analyze");
+		text = in.nextLine();
+		
+		while(!text.isEmpty()) {
+			letters.add(new LetterOccurance(text.charAt(0),text));
+			text = text.replace(String.valueOf(text.charAt(0)), "");
+		}
+		
+		Collections.sort(letters);
+		
+		for(LetterOccurance l : letters)
+			System.out.println(l);
+	}
+
+}
---
+++ fredrikOppgave/LetterOccurance.java
@@ -0,0 +1,38 @@
+package fredrikOppgave;
+
+public class LetterOccurance implements Comparable<LetterOccurance> {
+	private char letter;
+	private int occurance;
+	
+	public LetterOccurance(char letter, String text) {
+		super();
+		this.letter = letter;
+		this.occurance = countLetter(text);
+	}
+
+	public int getOccurance() {
+		return occurance;
+	}
+
+	public void setOccurance(int occurance) {
+		this.occurance = occurance;
+	}
+
+	public char getLetter() {
+		return letter;
+	}
+	
+	public int countLetter(String text) {
+		return text.length() - text.replace(String.valueOf(letter), "").length();
+	}
+	
+	@Override
+	public String toString() {
+		return String.valueOf(letter) + ": " + occurance + " times.";
+	}
+	
+	public int compareTo(LetterOccurance compareLetters) {
+		return this.letter - compareLetters.getLetter();
+	}
+
+}
